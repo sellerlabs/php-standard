5c6ce080004d91194a7f6daa248a7b0c7a7f0389
Eduardo Trujillo <ed@chromabits.com>
2015-06-21 17:23:41 -0400

    Add sniff for checking the @package annotation

--- src/Chromabits/Standards/Chroma/Sniffs/Commenting/ClassCommentSniff.php
+++ src/Chromabits/Standards/Chroma/Sniffs/Commenting/ClassCommentSniff.php
@@ -23,45 +23,133 @@ class Chroma_Sniffs_Commenting_ClassCommentSniff extends BaseSniff
             'required' => false,
             'allow_multiple' => false,
         ],
-       '@author' => [
+        '@author' => [
             'required' => true,
             'allow_multiple' => true,
         ],
-       '@copyright' => [
+        '@copyright' => [
             'required' => false,
             'allow_multiple' => true,
         ],
-       '@license' => [
+        '@license' => [
             'required' => false,
             'allow_multiple' => false,
         ],
-       '@version' => [
+        '@version' => [
             'required' => false,
             'allow_multiple' => false,
         ],
-       '@link' => [
+        '@link' => [
             'required' => false,
             'allow_multiple' => true,
         ],
-       '@see' => [
+        '@see' => [
             'required' => false,
             'allow_multiple' => true,
         ],
-       '@since' => [
+        '@since' => [
             'required' => false,
             'allow_multiple' => false,
         ],
-       '@deprecated' => [
+        '@deprecated' => [
             'required' => false,
             'allow_multiple' => false,
         ],
-       '@package' => [
+        '@package' => [
             'required' => true,
             'allow_multiple' => false,
         ],
-       '@subpackage' => [
+        '@subpackage' => [
             'required' => false,
             'allow_multiple' => false,
         ],
     ];
+
+    /**
+     * Extract the first namespace found in the file.
+     *
+     * @param PHP_CodeSniffer_File $file
+     *
+     * @return string
+     */
+    protected function extractNamespace(
+        PHP_CodeSniffer_File $file
+    ) {
+        $namespace = '';
+
+        $tokens = $file->getTokens();
+
+        $prev = $file->findNext(T_NAMESPACE, 0);
+
+        for ($i = $prev + 2; $i < count($tokens); $i++) {
+            if (!in_array($tokens[$i]['code'], [T_STRING, T_NS_SEPARATOR])) {
+                break;
+            }
+
+            $namespace .= $tokens[$i]['content'];
+        }
+
+        return $namespace;
+    }
+
+    /**
+     * Process the package tag.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param array $tags The tokens for these tags.
+     *
+     * @return void
+     */
+    protected function processPackage(
+        PHP_CodeSniffer_File $phpcsFile,
+        array $tags
+    ) {
+        $tokens = $phpcsFile->getTokens();
+        foreach ($tags as $tag) {
+            if ($tokens[($tag + 2)]['code'] !== T_DOC_COMMENT_STRING) {
+                // No content.
+                continue;
+            }
+
+            $namespace = $this->extractNamespace($phpcsFile);
+            $content = $tokens[($tag + 2)]['content'];
+
+            if ($namespace !== $content) {
+                $phpcsFile->addError(
+                    'Package name "%s" should be "%s"',
+                    $tag,
+                    'InvalidPackage',
+                    [
+                        $content,
+                        $namespace
+                    ]
+                );
+                continue;
+            }
+
+            if (PHP_CodeSniffer::isUnderscoreName($content) === true) {
+                continue;
+            }
+
+            $newContent = str_replace(' ', '_', $content);
+            $newContent = trim($newContent, '_');
+            $newContent = preg_replace('/[^A-Za-z_]/', '', $newContent);
+            $nameBits = explode('_', $newContent);
+            $firstBit = array_shift($nameBits);
+            $newName = strtoupper($firstBit{0}) . substr($firstBit, 1) . '_';
+            foreach ($nameBits as $bit) {
+                if ($bit !== '') {
+                    $newName .= strtoupper($bit{0}) . substr($bit, 1) . '_';
+                }
+            }
+
+            $error = 'Package name "%s" is not valid; consider "%s" instead';
+            $validName = trim($newName, '_');
+            $data = [
+                $content,
+                $validName,
+            ];
+            $phpcsFile->addError($error, $tag, 'InvalidPackage', $data);
+        }
+    }
 }
